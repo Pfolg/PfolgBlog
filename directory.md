465e736901efd365dd7ad97a3d23696e309bc1d3
Pfolg <2146047622@qq.com>
2025-03-27 22:44:48 +0800

    Feature：使用脚本整理文章

---
+++ directory.md
@@ -0,0 +1,110 @@
+# 目录
+
+### 2025年3月26日小记
+
+**创建时间：2025-03-26 19:07:49**
+
+[2025年3月26日小记](Passages/2025年3月26日小记/main.md)
+
+### 关闭动画以提升Windows11性能
+
+**创建时间：2025-03-23 00:05:34**
+
+[Windows性能提升之关闭动画](Passages/关闭动画以提升Windows11性能/Windows性能提升之关闭动画.md)
+
+### Win11-快捷托盘&激活水印
+
+**创建时间：2025-03-17 12:21:13**
+
+[Win11-快捷托盘&激活水印](Passages/Win11-快捷托盘&激活水印/main.md)
+
+### 学习笔记——QTableWidget
+
+**创建时间：2025-03-02 15:30:56**
+
+[学习笔记——QTableWidget](Passages/学习笔记——QTableWidget/main.md)
+
+### Windows重装&蓝屏&驱动&neofetch
+
+**创建时间：2025-02-15 09:09:19**
+
+[Windows重装&蓝屏&驱动&neofetch](Passages/Windows重装&蓝屏&驱动&neofetch/main.md)
+
+### 2024年12月26日前
+
+**创建时间：2025-02-14 20:05:36**
+
+[fortran安装](Passages/2024年12月26日前/fortran安装.md)
+
+[NirCmd-Gui-Chinese-Introduction](Passages/2024年12月26日前/NirCmd-Gui-Chinese-Introduction.md)
+
+[vscode背景更改](Passages/2024年12月26日前/vscode背景更改.md)
+
+[如何在IDEA中使用RainbowFart](Passages/2024年12月26日前/如何在IDEA中使用RainbowFart.md)
+
+[实用软件记录](Passages/2024年12月26日前/实用软件记录.md)
+
+[开源软件分享](Passages/2024年12月26日前/开源软件分享.md)
+
+[快速认证校园网（限CUIT）](Passages/2024年12月26日前/快速认证校园网（限CUIT）.md)
+
+### 2024年12月26日概要
+
+**创建时间：2025-02-14 20:05:36**
+
+[2024年12月26日概要](Passages/2024年12月26日概要/main.md)
+
+### Fortran安装gcc+VScode+Python
+
+**创建时间：2025-02-14 20:05:36**
+
+[Fortran安装gcc+VScode+Python](Passages/Fortran安装gcc+VScode+Python/Fortran安装gcc+VScode+Python.md)
+
+### Fortran考前复习
+
+**创建时间：2025-02-14 20:05:36**
+
+[复习](Passages/Fortran考前复习/复习.md)
+
+### HowToInstallNetEaseMusicInLinux
+
+**创建时间：2025-02-14 20:05:36**
+
+[howToInstallNetEaseMusicInLinux](Passages/HowToInstallNetEaseMusicInLinux/howToInstallNetEaseMusicInLinux.md)
+
+### Linux定时关机程序
+
+**创建时间：2025-02-14 20:05:36**
+
+[Linux定时关机程序](Passages/Linux定时关机程序/main.md)
+
+### 云降水物理复习
+
+**创建时间：2025-02-14 20:05:36**
+
+[云降水物理复习](Passages/云降水物理复习/云降水物理复习.md)
+
+### 利用玄学解决ubuntu没有WiFi
+
+**创建时间：2025-02-14 20:05:36**
+
+[利用玄学解决ubuntu没有WiFi](Passages/利用玄学解决ubuntu没有WiFi/main.md)
+
+### 发现了一个有趣的开源项目-BrowsingYearReview
+
+**创建时间：2025-02-14 20:05:36**
+
+[发现了一个有趣的开源项目-BrowsingYearReview](Passages/发现了一个有趣的开源项目-BrowsingYearReview/main.md)
+
+### 自动点击（python）
+
+**创建时间：2025-02-14 20:05:36**
+
+[自动点击（python）](Passages/自动点击（python）/main.md)
+
+### 解决linux上没有好用的pdf编辑器
+
+**创建时间：2025-02-14 20:05:36**
+
+[解决linux上没有好用的pdf编辑器](Passages/解决linux上没有好用的pdf编辑器/main.md)
+
---
+++ format.py
@@ -0,0 +1,86 @@
+import os
+import datetime
+
+directory="./directory.md"
+
+def getFileCreatTime(p):
+    # 获取创建时间戳（Windows上为创建时间）
+    if os.path.exists(p):
+        creation_timestamp = os.path.getctime(p)
+
+        # 转换为可读时间格式
+        creation_time = datetime.datetime.fromtimestamp(creation_timestamp)
+        formatted_time = creation_time.strftime("%Y-%m-%d %H:%M:%S")
+
+        return formatted_time
+    else:
+        return None
+
+def sortData(data):
+    # 定义当前时间
+    now = datetime.datetime.now()
+    # 转换为可排序的列表（保留键值关系）
+    sorted_items = sorted(
+        data.items(),
+        key=lambda item: abs(now - datetime.datetime.strptime(item[1]['time'], "%Y-%m-%d %H:%M:%S"))
+    )
+
+    # 输出排序结果（按时间由近到远）
+    # for key, value in sorted_items:
+    #     print(f"{key}: {value['time']}")
+
+    # 如果需要生成新的有序字典
+    from collections import OrderedDict
+    ordered_data = OrderedDict(sorted_items)
+    return ordered_data
+
+passages_infor={}
+print("正在读取文章数据……")
+for i,j,k in os.walk("Passages"):
+    # print("\ni="+str(i)+"\nj="+str(j)+"\nk="+str(k))
+    if i=="Passages":
+        for a in j:
+            passages_infor[a]={
+            "folder":i+"/"+a,
+            "time":getFileCreatTime(i+"/"+a)
+        }
+# print("-"*50)
+print("文章获取完毕，进一步分析……")
+for item in passages_infor.keys():
+    folder=passages_infor.get(item).get("folder")
+    for i,j,k in os.walk(folder):
+        # print("\ni="+str(i)+"\nj="+str(j)+"\nk="+str(k))
+        if "\\" in i:
+            continue
+        elif k:
+            psg=[]
+            for p in k:
+                if ".md" in p:
+                    psg.append(folder+"/"+p)
+            psg_dict=passages_infor.get(item)
+            psg_dict["psg"]=psg
+            passages_infor[item]=psg_dict
+
+
+
+print("文章数据分析完毕……\n开始自动整理目录……")
+# print(passages_infor)
+passages_infor=sortData(passages_infor)
+# 清空原数据
+with open(directory,"w",encoding="utf-8") as file:
+    file.write("# 目录\n\n")
+
+msg=""
+with open(directory,"a",encoding="utf-8") as file:
+    for i in passages_infor.keys():
+        file.write(f"### {i}\n\n**创建时间：{passages_infor.get(i).get('time')}**\n\n")
+        psg=passages_infor.get(i).get("psg")
+        for j in psg:
+            title=j.replace('/','.').split('.')[-2]
+            if title in ["main"]:
+                title=i
+            file.write(f"[{title}]({j})\n\n")
+        if " " in j:
+            msg="\n如果路径包含空格可能无法生成正常链接！"
+
+print("目录文件生成完毕！",msg)
